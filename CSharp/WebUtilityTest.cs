349dfdaf427d5dcc3b5f7f765f2c085d00f335d8
Sofia <aconnor0213+sofia@gmail.com>
2025-03-05 13:44:24 +0000

    Added AboutComnand Test

--- CSharp/WebUtilityTest.cs
+++ CSharp/WebUtilityTest.cs
@@ -4,7 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        string htmlCode = """
+      HelpCommand();
+    }
+    
+    private void HelpCommand()
+    {
+      string htmlCode = """
             <h4>Midnight Help Menu</h4><br>
             <p>!quote | usage !quote <username><br>!help | Shows this help menu </p>
         """;
@@ -13,5 +18,17 @@ class Program
         string htmlDecode = WebUtility.HtmlDecode(htmlEncode);
 
         Console.WriteLine($"Printed html code: \n{htmlDecode}");
+    }
+    
+    private void AboutCommand()
+    {
+      string htmlCode1 = """
+        <p>About Command</p>
+      """;
+      
+      string htmlEncode1 = WebUtility.HtmlEncode(htmlCode1);
+      string htmlDecode1 = WebUtility.HtmlDecode(htmlEncode1);
+      
+      Console.WriteLine($"Printed html code: \n{htmlDecode1}");
     }
 }
